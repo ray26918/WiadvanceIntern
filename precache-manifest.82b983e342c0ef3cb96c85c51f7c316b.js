d720549eba5bd8afc1938193c97731333a8b7d16
Ray <Ray.Wu@wiadvance.com>
2021-06-15 16:17:10 +0800

    Updates

---
+++ precache-manifest.82b983e342c0ef3cb96c85c51f7c316b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98e543469adb2b2d6315",
+    "url": "/static/css/main.ec49bcef.chunk.css"
+  },
+  {
+    "revision": "98e543469adb2b2d6315",
+    "url": "/static/js/main.98e54346.chunk.js"
+  },
+  {
+    "revision": "b05a1265c049540b3f72",
+    "url": "/static/js/1.b05a1265.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e50234a90c19a3b83a219644a2ba0d07",
+    "url": "/index.html"
+  }
+];
